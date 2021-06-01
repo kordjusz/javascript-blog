a7cc1a78b6b4479d4c964d11b2667cd3c732418f
Kordian Juszkiewicz <kordjusz@gmail.com>
2021-06-01 22:03:51 +0200

    Finish all JS functions responsible for sorting articles according to authoes and tags

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,10 @@ function titleClickHandler(event){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optArticleTagsSelector = '.post-tags .list',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleAuthorSelector = '.post-author',
+  optArticleAuthorSelectorA = '.post-author a',
+  optArticleTagsSelectorA = '.post-tags .list li a';
 
 function generateTitleLinks(customSelector = ''){
 
@@ -69,28 +72,42 @@ function generateTags(){
     const articleTagsArray = articleTags.split(' ');
 
     for(let tag of articleTagsArray){
-
-      const tagHTML = `<li><a href="#${tag}">${tag}</a></li>`;
+      const tagHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
+      //const tagHTML = '<li><a href="#tag-'+ tag + '">' + tag + '</a></li>';
       html = html + tagHTML;
     }
-    wrapperTags.insertAdjacentHTML('beforeend', html);
+    wrapperTags.insertAdjacentHTML('afterbegin', html);
   }
 }
 generateTags();
+
+function generateAuthors(){
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for(let article of articles){
+    const authorName = article.getAttribute('data-author');
+    const authorLink = `<a href="#${authorName}"> by ${authorName}</a>`;
+    const postAuthor = article.querySelector(optArticleAuthorSelector);
+    postAuthor.innerHTML = authorLink;
+  }
+}
+generateAuthors();
+
+
 
 function tagClickHandler(event){
   /* prevent default action for this event */
   event.preventDefault();
   /* make new constant named "clickedElement" and give it the value of "this" */
   const clickedElement = this;
-  console.log(clickedElement);
   /* make a new constant "href" and read the attribute "href" of the clicked element */
   const href = clickedElement.getAttribute('href');
   console.log(href);
   /* make a new constant "tag" and extract tag from the "href" constant */
-  const tag = href.replace('#', '');
+  const tag = href.replace('#tag-', '');
+  console.log(tag);
   /* find all tag links with class active */
-  const activeLinks = document.querySelectorAll(href);
+  const activeLinks = document.querySelectorAll('.post-tags .list .active');
   /* START LOOP: for each active tag link */
   for(let activeLink of activeLinks){
   /* remove class active */
@@ -98,7 +115,7 @@ function tagClickHandler(event){
   /* END LOOP: for each active tag link */
   }
   /* find all tag links with "href" attribute equal to the "href" constant */
-  const showLinks = document.querySelectorAll('a.[href="#' + href + '"]');
+  const showLinks = document.querySelectorAll('.post-tags .list [href="' + href + '"]');
   /* START LOOP: for each found tag link */
   for(let showLink of showLinks){
   /* add class active */
@@ -112,15 +129,38 @@ function tagClickHandler(event){
 
 function addClickListenersToTags(){
   /* find all links to tags */
-  const tagLinks = document.querySelectorAll(optArticleTagsSelector);
+  const tagLinks = document.querySelectorAll(optArticleTagsSelectorA);
   /* START LOOP: for each link */
   for(let tagLink of tagLinks){
-  /* add tagClickHandler as event listener for that link */
+    /* add tagClickHandler as event listener for that link */
     tagLink.addEventListener('click', tagClickHandler);
   /* END LOOP: for each link */
   }
 }
 
-//addClickListenersToTags();
+addClickListenersToTags();
+
+function authorClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
+  const href = clickedElement.getAttribute('href');
+  const tag = href.replace('#', '');
+  console.log(tag);
+
+  generateTitleLinks('[data-author="' + tag + '"]');
+}
+
+function addClickListenersToAuthors(){
+  const postAuthors = document.querySelectorAll(optArticleAuthorSelectorA);
+
+  for(let author of postAuthors){
+    author.addEventListener('click', authorClickHandler);
+  }
+}
+
+addClickListenersToAuthors();
 
 
+// function testFunction(){
+//   console.log('correct');
+// }
